969c8bd0def467f4586aea58ee4a60751b594c4c
Antoine Diffloth <adiffloth@gmail.com>
2024-10-27 14:11:26 -0700

    initial load balancing client and server

---
+++ lb_client.py
@@ -0,0 +1,30 @@
+import grpc
+import streaming_pb2
+import streaming_pb2_grpc
+import time
+
+def generate_data_chunks(client_id):
+    # Simulate sending a stream of data
+    for i in range(1, 21):  # Send 20 chunks of data
+        print(f'Sending chunk {i}')
+        yield streaming_pb2.DataChunk(
+            content=f"Client {client_id} sent chunk {i}",
+            sequence_number=i
+        )
+        time.sleep(1)
+
+def run():
+    server_ip = input("Enter the server IP address: ") # Get the server IP address from the user
+    client_id = input("Enter the client ID: ") # Get the client ID from the user
+    # Open a gRPC channel to the server
+    with grpc.insecure_channel(f'{server_ip}:50051') as channel:
+        stub = streaming_pb2_grpc.DataProcessingServiceStub(channel)
+        
+        # Send data stream and receive occasional responses
+        response_iterator = stub.ProcessDataStream(generate_data_chunks(client_id))
+        
+        for response in response_iterator:
+            print(f"Received response from server: {response.message}, processed sequence: {response.processed_sequence}")
+
+if __name__ == "__main__":
+    run()
---
+++ lb_server.py
@@ -0,0 +1,30 @@
+import grpc
+from concurrent import futures
+import time
+import streaming_pb2
+import streaming_pb2_grpc
+
+# Implement the service defined in the proto file
+class DataProcessingServiceServicer(streaming_pb2_grpc.DataProcessingServiceServicer):
+
+    def ProcessDataStream(self, request_iterator, context):
+        for i, data_chunk in enumerate(request_iterator):
+            # Simulate processing the incoming data chunk
+            print(f"Received chunk {data_chunk.sequence_number}: {data_chunk.content}")
+            yield streaming_pb2.ServerResponse(
+                    message=f"Processed {data_chunk.content}",
+                    processed_sequence=data_chunk.sequence_number
+                )
+
+
+# Start the server
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    streaming_pb2_grpc.add_DataProcessingServiceServicer_to_server(DataProcessingServiceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("Server started on port 50051")
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
