4ad94a0ca3c005361c84c49594bfa2c1b0b437c1
Antoine Diffloth <adiffloth@gmail.com>
2024-10-27 15:10:11 -0700

    Add parameter for number of clients. Add request counter.

--- lb_mt_client.py
+++ lb_mt_client.py
@@ -5,23 +5,23 @@ import time
 import sys
 from concurrent.futures import ThreadPoolExecutor
 
-def generate_data_chunks(client_id):
+def generate_data_chunks(client_id, num_chunks):
     # Simulate sending a stream of data
-    for i in range(1, 21):  # Send 20 chunks of data
-        print(f'Sending chunk {i}')
+    for i in range(1, num_chunks+1):  # Send 20 chunks of data
+        print(f'Client {client_id} sending chunk {i}')
         yield streaming_pb2.DataChunk(
             content=client_id,
             sequence_number=i
         )
         time.sleep(1)
 
-def run_client(server_ip, client_id):
+def run_client(server_ip, client_id, num_chunks):
     # Open a gRPC channel to the server
     with grpc.insecure_channel(f'{server_ip}:50051') as channel:
         stub = streaming_pb2_grpc.DataProcessingServiceStub(channel)
         
         # Send data stream and receive occasional responses
-        response_iterator = stub.ProcessDataStream(generate_data_chunks(client_id))
+        response_iterator = stub.ProcessDataStream(generate_data_chunks(client_id, num_chunks))
         
         for response in response_iterator:
             print(f"Client {client_id} Response: {response.message}, {response.processed_sequence}")
@@ -30,13 +30,15 @@ def run_client(server_ip, client_id):
 def main():
     server_ip = sys.argv[1]  # Get the server IP address from command-line arguments
     num_clients = int(sys.argv[2])  # Number of clients to start
+    num_chunks = int(sys.argv[3])  # Number of chunks to send per client
 
     # Use ThreadPoolExecutor to manage multiple client threads
     with ThreadPoolExecutor(max_workers=num_clients) as executor:
         futures = []
         for i in range(num_clients):
-            client_id = f"client_{i + 1}"
-            futures.append(executor.submit(run_client, server_ip, client_id))
+            client_id = f"{i + 1}"
+            futures.append(executor.submit(run_client, server_ip, client_id, num_chunks))
+            time.sleep(1/num_clients)
 
         # Wait for all threads to complete
         for future in futures:
--- lb_server.py
+++ lb_server.py
@@ -1,16 +1,24 @@
 import grpc
 from concurrent import futures
-import time
 import streaming_pb2
 import streaming_pb2_grpc
+import threading
 
 # Implement the service defined in the proto file
 class DataProcessingServiceServicer(streaming_pb2_grpc.DataProcessingServiceServicer):
 
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.counter = 0
+
     def ProcessDataStream(self, request_iterator, context):
-        for i, data_chunk in enumerate(request_iterator):
+        for data_chunk in request_iterator:
+            with self.lock:
+                self.counter += 1
+                current_counter = self.counter
+
             # Simulate processing the incoming data chunk
-            print(f"Received seq {data_chunk.sequence_number} from client {data_chunk.content}")
+            print(f"{self.counter} Received seq {data_chunk.sequence_number} from client {data_chunk.content}")
             yield streaming_pb2.ServerResponse(
                     message=f"Processed {data_chunk.content}-{data_chunk.sequence_number}",
                     processed_sequence=data_chunk.sequence_number
